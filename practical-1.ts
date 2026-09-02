3a249ab7e60913605e1f4dcb5b9e370d6ad930ac
Saikat Sarkar <me.saikatsarkar.dev@gmail.com>
2026-09-02 13:21:39 +0530

    first practical done

---
+++ practical-1.ts
@@ -0,0 +1,9 @@
+let fullName: string = "Saikat Sarkar";
+let age: string = "18";
+let gender: string = "Male";
+let phoneNumber: string = "5965565456";
+
+console.log(fullName);
+console.log(age);
+console.log(gender);
+console.log(phoneNumber);
